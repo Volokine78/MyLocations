c96a8af98b645d2fe46e0de49fff5539df16f835
Tolga Pirtürk <tpirturk@gmail.com>
2021-04-29 00:57:30 +0300

    Add prepare(for:sender:) method for make a segue to LocationDetailsViewController user pick a category.

--- MyLocations/CategoryPickerViewController.swift
+++ MyLocations/CategoryPickerViewController.swift
@@ -38,6 +38,16 @@ class CategoryPickerViewController: UITableViewController {
         }
     }
     
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "PickedCategory" {
+            let cell = sender as! UITableViewCell
+            if let indexPath = tableView.indexPath(for: cell) {
+                selectedCategoryName = categories[indexPath.row]
+            }
+        }
+    }
+    
     // MARK: - Table View Delegates
     override func tableView(
         _ tableView: UITableView,
