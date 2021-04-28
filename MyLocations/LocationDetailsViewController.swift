061bad71c70269ab50141c94ef2c91089fd97570
Tolga Pirtürk <tpirturk@gmail.com>
2021-04-29 00:27:23 +0300

    Add categoryName variable and change category label text. Add prepare(for:,sender:) method to create segue to CategoryPickerViewController.

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -21,12 +21,13 @@ class LocationDetailsViewController: UITableViewController {
     
     var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
     var placemark: CLPlacemark?
+    var categoryName = "No Category"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         descriptionTextView.text = ""
-        categoryLabel.text = ""
+        categoryLabel.text = categoryName
         
         latitudeLabel.text = String(format: "%.8f", coordinate.latitude)
         longitudeLabel.text = String(format: "%.8f", coordinate.longitude)
@@ -46,6 +47,14 @@ class LocationDetailsViewController: UITableViewController {
     @IBOutlet var longitudeLabel: UILabel!
     @IBOutlet var addressLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "PickCategory" {
+            let controller = segue.destination as! CategoryPickerViewController
+            controller.selectedCategoryName = categoryName
+        }
+    }
     
     // MARK: - Helper Methods
     func string(from placemark: CLPlacemark) -> String {
