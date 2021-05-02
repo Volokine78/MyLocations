82e471af65c98ad6f1cc5447f53121a62452f7d8
Tolga Pirtürk <tpirturk@gmail.com>
2021-05-02 03:22:03 +0300

    Add Location entity to MyLocations data model with attributes. Change Location entity's Codegen attribute to Manual/None.

---
+++ Location+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Tolga PIRTURK on 2.05.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
