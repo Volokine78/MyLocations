81f63da02d06376e961ea98ba52fc7f2930811ee
Tolga Pirtürk <tpirturk@gmail.com>
2021-05-05 03:51:53 +0300

    Change tableView(_:cellForRowAt:) method to give the cell the reference of LocationCell object and add configure(for:) method which defined in LocationCell to configure descriptionLabel and addressLabel text.

--- MyLocations/LocationCell.swift
+++ MyLocations/LocationCell.swift
@@ -22,5 +22,30 @@ class LocationCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    // MARK: - Helper Methods
+    func configure(for location: Location) {
+        if location.locationDescription.isEmpty {
+            descriptionLabel.text = "(No Description)"
+        } else {
+            descriptionLabel.text = location.description
+        }
+        
+        if let placemark = location.placemark {
+            var text = ""
+            if let tmp = placemark.subThoroughfare {
+                text += tmp + " "
+            }
+            if let tmp = placemark.thoroughfare {
+                text += tmp + ", "
+            }
+            if let tmp = placemark.locality {
+                text += tmp
+            }
+            addressLabel.text = text
+        } else {
+            addressLabel.text = String(format: "Lat: %8f, Long: %8f",
+                                       location.latitude,
+                                       location.longitude)
+        }
+    }
 }
