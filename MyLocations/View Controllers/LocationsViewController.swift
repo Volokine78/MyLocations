5f3d0da08c37d48971201a8ac6ed98ed029abc25
Tolga Pirtürk <tpirturk@gmail.com>
2021-05-05 17:53:02 +0300

    Add tableView(_:commit:for:rowAt:) method for to add deletion feature on locations tab.

--- MyLocations/View Controllers/LocationsViewController.swift
+++ MyLocations/View Controllers/LocationsViewController.swift
@@ -78,6 +78,22 @@ class LocationsViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(
+        _ tableView: UITableView,
+        commit editingStyle: UITableViewCell.EditingStyle,
+        forRowAt indexPath: IndexPath
+    ) {
+        if editingStyle == .delete {
+            let location = fetchedResultsController.object(at: indexPath)
+            managedObjectContext.delete(location)
+            do {
+                try managedObjectContext.save()
+            } catch {
+                fatalCoreDataError(error)
+            }
+        }
+    }
+    
     // MARK: - Helper Methods
     func performFetch() {
         do {
