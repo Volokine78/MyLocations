1f66bc75188ce447809d3c231df521a05faa8152
Tolga Pirtürk <tpirturk@gmail.com>
2021-05-05 18:13:50 +0300

    Modify fetchedResultsController to be able to sort location objects also with category. Add numberOfSections(in:) and tableView(_:titleForHeaderInSection:) Table View Delegate methods for sections.

--- MyLocations/View Controllers/LocationsViewController.swift
+++ MyLocations/View Controllers/LocationsViewController.swift
@@ -18,15 +18,16 @@ class LocationsViewController: UITableViewController {
         let entity = Location.entity()
         fetchRequest.entity = entity
         
-        let sortDescriptor = NSSortDescriptor(key: "date", ascending: true)
-        fetchRequest.sortDescriptors = [sortDescriptor]
+        let sort1 = NSSortDescriptor(key: "category", ascending: true)
+        let sort2 = NSSortDescriptor(key: "date", ascending: true)
+        fetchRequest.sortDescriptors = [sort1, sort2]
         
         fetchRequest.fetchBatchSize = 20
         
         let fetchedResultsController = NSFetchedResultsController(
             fetchRequest: fetchRequest,
             managedObjectContext: self.managedObjectContext,
-            sectionNameKeyPath: nil,
+            sectionNameKeyPath: "category",
             cacheName: "Locations")
         
         fetchedResultsController.delegate = self
@@ -63,6 +64,20 @@ class LocationsViewController: UITableViewController {
     ) -> Int {
         let sectionInfo = fetchedResultsController.sections![section]
         return sectionInfo.numberOfObjects
+    }
+    
+    override func numberOfSections(
+        in tableView: UITableView
+    ) -> Int {
+        return fetchedResultsController.sections!.count
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        titleForHeaderInSection section: Int
+    ) -> String? {
+        let sectionInfo = fetchedResultsController.sections![section]
+        return sectionInfo.name
     }
     
     override func tableView(
