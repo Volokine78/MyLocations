22ddcf1a5f26d817655745edef32a0962865f5f7
Tolga Pirtürk <tpirturk@gmail.com>
2021-05-08 04:41:19 +0300

    Add prepare(for:sender:) method and modify showLocationDetails(_:) method to perform segue to edit Locations screen.

--- MyLocations/View Controllers/MapViewController.swift
+++ MyLocations/View Controllers/MapViewController.swift
@@ -25,6 +25,18 @@ class MapViewController: UIViewController {
         }
     }
     
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EditLocation" {
+            let controller = segue.destination as! LocationDetailsViewController
+            controller.managedObjectContext = managedObjectContext
+            
+            let button = sender as! UIButton
+            let location = locations[button.tag]
+            controller.locationToEdit = location
+        }
+    }
+    
     // MARK: - Actions
     @IBAction func showUser() {
         let region = MKCoordinateRegion(
@@ -109,7 +121,7 @@ class MapViewController: UIViewController {
     }
     
     @objc func showLocationDetails(_ sender: UIButton) {
-        
+        performSegue(withIdentifier: "EditLocation", sender: sender)
     }
 }
 
