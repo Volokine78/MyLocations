b453d2d0a2bf9353947ee8bf8719ff2d7127b562
Tolga Pirtürk <tpirturk@gmail.com>
2021-05-08 03:24:21 +0300

    Add region(for:) method to handle situations due to the number of tagged locations for choose which location map view will display on screen.

--- MyLocations/View Controllers/MapViewController.swift
+++ MyLocations/View Controllers/MapViewController.swift
@@ -45,6 +45,62 @@ class MapViewController: UIViewController {
         locations = try! managedObjectContext.fetch(fetchRequest)
         mapView.addAnnotations(locations)
     }
+    
+    func region(for annotations: [MKAnnotation]) -> MKCoordinateRegion {
+        let region: MKCoordinateRegion
+        
+        switch annotations.count {
+            case 0:
+                region = MKCoordinateRegion(
+                    center: mapView.userLocation.coordinate,
+                    latitudinalMeters: 1000,
+                    longitudinalMeters: 1000)
+            case 1:
+                let annotation = annotations[annotations.count - 1]
+                region = MKCoordinateRegion(
+                    center: annotation.coordinate,
+                    latitudinalMeters: 1000,
+                    longitudinalMeters: 1000)
+            default:
+                var topleft = CLLocationCoordinate2D(
+                    latitude: -90,
+                    longitude: 180)
+                var bottomRight = CLLocationCoordinate2D(
+                    latitude: 90,
+                    longitude: -180)
+                
+                for annotation in annotations {
+                    topleft.latitude = max(
+                        topleft.latitude,
+                        annotation.coordinate.latitude)
+                    topleft.longitude = min(
+                        topleft.longitude,
+                        annotation.coordinate.longitude)
+                    bottomRight.latitude = min(
+                        bottomRight.latitude,
+                        annotation.coordinate.latitude)
+                    bottomRight.longitude = max(
+                        bottomRight.longitude,
+                        annotation.coordinate.longitude)
+                }
+                
+                let center = CLLocationCoordinate2D(
+                    latitude: topleft.latitude -
+                        (topleft.latitude - bottomRight.latitude / 2),
+                    longitude: topleft.longitude -
+                        (topleft.longitude - bottomRight.longitude / 2))
+                
+                let extraSpace = 1.1
+                let span = MKCoordinateSpan(
+                    latitudeDelta: abs(topleft.latitude - bottomRight.latitude)
+                        * extraSpace,
+                    longitudeDelta: abs(topleft.longitude - bottomRight.longitude)
+                        * extraSpace)
+                
+                region = MKCoordinateRegion(center: center, span: span)
+        }
+        return mapView.regionThatFits(region)
+    }
 }
 
 extension MapViewController: MKMapViewDelegate{
