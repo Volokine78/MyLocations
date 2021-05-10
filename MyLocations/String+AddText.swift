25eef1000f0ca1f7b06504a8606aaf2aa24e2f19
Tolga Pirtürk <tpirturk@gmail.com>
2021-05-10 22:36:26 +0300

    Add Sting+AddText swift file to My Locations project on project navigator. Add add(text:seperatedBy:) method to Strind struct by extension.

---
+++ MyLocations/String+AddText.swift
@@ -0,0 +1,19 @@
+//
+//  String+AddText.swift
+//  MyLocations
+//
+//  Created by Tolga PIRTURK on 10.05.2021.
+//
+
+import Foundation
+
+extension String {
+    mutating func add(text: String?, seperatedBy seperator: String) {
+        if let text = text {
+            if !isEmpty {
+                self += seperator
+            }
+            self += text
+        }
+    }
+}
